eb8bb525a559981a72569f9ff995c62085f1093e
Nitesh kumar <136956056+Nitesh060@users.noreply.github.com>
2026-05-11 12:43:36 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,32 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from farmscore import calculate_farm_score
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return "Farm Score API is Running"
 
 @app.route('/calculate', methods=['POST'])
-
 def calculate():
 
     data = request.json
 
-    lat = data['lat']
-    lon = data['lon']
+    lat = float(data['lat'])
+    lon = float(data['lon'])
 
-    # Temporary sample values
+    # TEMPORARY SAMPLE VALUES
+    # Later replace with Earth Engine data
+
     gw = 32
     ndvi = 72
     ndmi = 55
     rainfall = 45
     temp = 36
 
-    final_score = calculate_farm_score(
+    result = calculate_farm_score(
         gw,
         ndvi,
         ndmi,
@@ -28,7 +35,16 @@ def calculate():
     )
 
     return jsonify({
-        "final_score": final_score
+        "latitude": lat,
+        "longitude": lon,
+        "groundwater": gw,
+        "ndvi": ndvi,
+        "ndmi": ndmi,
+        "rainfall": rainfall,
+        "temperature": temp,
+        "rainfall_score": result["rainfall_score"],
+        "temperature_score": result["temperature_score"],
+        "final_score": result["final_score"]
     })
 
 if __name__ == '__main__':
