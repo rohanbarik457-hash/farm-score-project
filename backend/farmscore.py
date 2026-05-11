cc25627796308a2a35921732151bc7177ae73e5d
Nitesh kumar <136956056+Nitesh060@users.noreply.github.com>
2026-05-11 12:44:06 +0530

    Update farmscore.py

--- backend/farmscore.py
+++ backend/farmscore.py
@@ -1,17 +1,25 @@
 def calculate_farm_score(gw, ndvi, ndmi, rainfall, temp):
 
-    rainfall_score = 100 - 100 * abs(50 - rainfall) / 50
+    rainfall_score = (
+        100 - 100 * abs(50 - rainfall) / 50
+    )
 
-    temp_score = 100 - 100 * abs(40 - temp) / 40
+    temperature_score = (
+        100 - 100 * abs(40 - temp) / 40
+    )
 
     weighted_avg = (
         10 * gw +
         30 * ndvi +
         25 * ndmi +
         10 * rainfall_score +
-        25 * temp_score
+        25 * temperature_score
     ) / 100
 
     final_score = round(weighted_avg) + 150
 
-    return final_score
+    return {
+        "rainfall_score": round(rainfall_score, 2),
+        "temperature_score": round(temperature_score, 2),
+        "final_score": final_score
+    }
