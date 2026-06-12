a824bb035465ed0a2e054c5d44a0905acac5ac79
Rohan Barik <rohanbarik457@gmail.com>
2026-06-12 09:53:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
 
 const API_BASE_URL =
     window.FARMSCORE_API_URL ||
-    "http://localhost:5000";
+    "https://farm-score-project-1xte.onrender.com";
 
 /**
  * Call the FarmScore backend to calculate an agricultural suitability score.
